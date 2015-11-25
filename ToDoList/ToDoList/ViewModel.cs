6165de8aeca6da14b3fc13524c6fd9c36fa34c7b
teejayt <tolanijayt4ril@yahoo.com>
2015-11-25 09:57:13 +0000

    Added Some methods

---
+++ ToDoList/ToDoList/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+  public  class ViewModel
+    {
+
+        private string _text;
+
+        public string text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        
+      public void AddItem(string text)
+      {
+
+      }
+
+      public void EditItem(string text)
+      {
+
+      }
+
+      public void MarkItem(string text)
+      {
+
+      }
+
+      public void RemoveItem(string text)
+      {
+
+      }
+    }
+}
