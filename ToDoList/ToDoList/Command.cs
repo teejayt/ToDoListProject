e81c93c4627b30ae745b130c0160f8b9a1b13c14
teejayt <tolanijayt4ril@yahoo.com>
2015-11-25 11:45:59 +0000

    Added Some Functionalities to the ViewModel and renamed The RemoveItem test to RemoveItemTest. The MainWindow.xaml has also been updated.

--- ToDoList/ToDoList/Command.cs
+++ ToDoList/ToDoList/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,21 +10,93 @@ namespace ToDoList
 {
     public class Command : ICommand
     {
-        public bool CanExecute(object parameter)
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action execute, Func<bool> canExecute = null)
         {
-            throw new NotImplementedException();
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute ?? (() => true);
         }
 
-        public event EventHandler CanExecuteChanged;
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute();
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            try
+            {
+                _execute();
+            }
+            catch
+            {
+                Debugger.Break();
+            }
         }
 
         public void RaiseCanExecuteChanged()
         {
+            CanExecuteChanged.Invoke(this, EventArgs.Empty);
+        }
+    }
 
+    public class Command<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<T> execute, Func<T, bool> canexecute = null)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canexecute ?? (e => true);
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object p)
+        {
+            try
+            {
+                var _Value = (T)Convert.ChangeType(p, typeof(T));
+                return _canExecute == null ? true : _canExecute(_Value);
+            }
+            catch { return false; }
+        }
+
+        public void Execute(object p)
+        {
+            if (!CanExecute(p))
+                return;
+            var _Value = (T)Convert.ChangeType(p, typeof(T));
+            _execute(_Value);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged.Invoke(this, EventArgs.Empty);
         }
     }
 }
