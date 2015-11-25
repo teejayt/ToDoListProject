2632733dd5d865be2de940ebf209eda7ee5fe729
jamesfreke <jamesfreke2808@gmail.com>
2015-11-25 12:49:18 +0000

    2nd Add Item Test

--- ToDoList/UnitTest/AddItemTest.cs
+++ ToDoList/UnitTest/AddItemTest.cs
@@ -26,5 +26,25 @@ namespace UnitTest
             //Assert
             Assert.AreEqual(expectedList.Count , viewmodel.newList.Count);
         }
+
+        [TestMethod]
+        public void Test_AddItem_AddsOneItemWhenPassedMoreThanOneItem()
+        {
+            //Arrange
+            ViewModel viewmodel = new ViewModel();
+            string value = "meeting";
+            string value2 = "lunch";
+            ObservableCollection<string> expectedList = new ObservableCollection<string>()
+            {
+                value
+            };
+
+            //Act
+            viewmodel.AddItem(value);
+            viewmodel.AddItem(value2);
+
+            //Assert
+            Assert.AreEqual(expectedList.Count, viewmodel.newList.Count);
+        }
     }
 }
