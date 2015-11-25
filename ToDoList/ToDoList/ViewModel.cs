5ccc2955a01f86430df7a0273c56aa26130fbf29
teejayt <tolanijayt4ril@yahoo.com>
2015-11-25 10:13:52 +0000

    Added more functionality to the ViewModel

--- ToDoList/ToDoList/ViewModel.cs
+++ ToDoList/ToDoList/ViewModel.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ToDoList
 {
-  public  class ViewModel
+  public  class ViewModel: INotifyPropertyChanged
     {
 
         private string _text;
-
         public string text
         {
             get { return _text; }
-            set { _text = value; }
+            set {
+                _text = value;
+                OnPropertyChanged("text");
+            
+            }
         }
-        
+
+        public ViewModel()
+        {
+
+        }
+
+        private ObservableCollection<string> newList;
       public void AddItem(string text)
       {
-
+          newList.Add(text);
       }
 
       public void EditItem(string text)
@@ -34,7 +45,16 @@ namespace ToDoList
 
       public void RemoveItem(string text)
       {
+          newList.Remove(text);
+      }
 
+      public event PropertyChangedEventHandler PropertyChanged;
+      private void OnPropertyChanged(string propertyName)
+      {
+          if (PropertyChanged != null)
+          {
+              PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+          }
       }
     }
 }
